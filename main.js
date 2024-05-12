05b668a815c0c4a672fe3a31c7e6870758beb8f4
Suzana Prado <115119746+SuzanaPrado2022@users.noreply.github.com>
2024-05-11 23:05:39 -0300

    while

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-let nomeUsuário = prompt("Qual o seu nome?");
+let nomeUsuário = '';
 let elemento = document.querySelector("#nome-usuário");
-elemento.textContent = nomeUsuário;
+
+
+while(nomeUsuário == ''){
+    nomeUsuário = prompt("Qual o seu nome?");
+    elemento.textContent = nomeUsuário;
+}
